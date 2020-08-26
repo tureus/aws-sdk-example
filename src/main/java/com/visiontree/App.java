0828a01f0e1de6439518284e7851a536708da6be
Xavier Lange <xlange@voh-b1-ln2-osx.hq.corp.viasat.com>
2020-08-26 01:42:46 -0400

    first commit

---
+++ src/main/java/com/visiontree/App.java
@@ -0,0 +1,26 @@
+package com.visiontree;
+
+import com.amazonaws.services.secretsmanager.*;
+import com.amazonaws.services.secretsmanager.model.ListSecretsRequest;
+import com.amazonaws.services.secretsmanager.model.ListSecretsResult;
+import com.amazonaws.services.secretsmanager.model.SecretListEntry;
+
+import java.util.List;
+
+// https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AWSSecretsManager ssm_builder = AWSSecretsManagerClient.builder().build();
+
+        ListSecretsRequest list_secrets = new ListSecretsRequest();
+        ListSecretsResult secrets_response = ssm_builder.listSecrets(list_secrets);
+        List<SecretListEntry> secrets = secrets_response.getSecretList();
+
+        for(int i = 0; i < secrets.size(); i++) {
+            SecretListEntry secret = secrets.get(i);
+            System.out.println(secret);
+        }
+    }
+}
